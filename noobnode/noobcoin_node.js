643251005e81bb0a073b344d4ddfeef60e038746
PreslavMihaylov <preslav@pmihaylov.com>
2018-02-10 21:51:03 +0200

    refactored noobnode

--- noobnode/noobcoin_node.js
+++ noobnode/noobcoin_node.js
@@ -93,39 +93,145 @@ var initHttpServer = () => {
         }
     });
 
-    app.get('/balance/:address', (req, res) => {
-        var address = req.params.address;
-        // TODO
+    app.get('/mining/get-block/:address', (req, res) => {
+
     });
 
-    app.post('/transactions/new', (req, res) => {
+    app.post('/mining/submit-block/:address', (req, res) => {
+        var minerAddress = req.params.address;
+        var miningJobIndex = node.miningJobs.map(
+            function(j) {
+                return j['address'];
+            }).indexOf(minerAddress);
+
+        node.miningJobs.splice(miningJobIndex, 1);
+
+        var isBlockAccepted = processBlock(req.body.data);
+
+        if(isBlockAccepted)
+        {
+            res.status(200).send('Block is accepted');
+        }
+        else
+        {
+            res.status(400).send('Block is not accepted');
+        }
+    });
+
+    app.get('/balance/:address/confirmations/:confirmations', (req, res) => {
+        var address = req.params.address;
         //TODO
     });
 
-    app.get('/transactions/:hash', (req,res) => {
-        var hash = req.params.hash;
-        // TODO:
-        /*if(allTransactions.some(t => t.hash = hash))
+    app.post('/transactions/new', (req, res) => {
+        var newTransaction = addPendingTransaction(req.body.data);
+        if (!newTransaction)
         {
-            res.send(JSON.stringify(allTransactions.find(t => t.hash = hash)));
+            res.status(400).send('Bad request');
+        }
+        else
+        {
+            node.pendingTransactions.push(newTransaction);
+            var response = {
+                "dateReceived": newTransaction.dateReceived,
+                "transactionHash": newTransaction.transactionHash
+            };
+
+            res.status(200).send(JSON.stringify(response));
+        }
+    });
+
+    app.get('/transactions/:hash/info', (req,res) => {
+        var hash = req.params.hash;
+        if(allTransactions.some(t => t.hash == hash))
+        {
+            res.send(JSON.stringify(allTransactions.find(t => t.hash == hash)));
         }
         else
         {
             res.status(404)
                 .send('Not found');
-        }*/
+        }
     });
 
     app.post('/blocks/notify', (req, res) => {
         //TODO
     });
 
-    app.get('/peers', (req, res) => res.send(JSON.stringify(node.peers)));
+    app.get('/peers', (req, res) => res.send(JSON.stringify(node.peers))); //TODO 409 code
     app.post('/peers', (req, res) => {
         //TODO
     });
 
     app.listen(http_port, () => console.log('Listening http on port: ' + http_port));
+}
+
+var addressHasEnoughMoney = (address, value) => {
+    return (node.balances['address'] >= value);
+}
+
+var validateKeys = (pubKey, signature) => {
+    //TODO validation of public key and signature
+}
+
+var validateAddresses = (fromAddress, toAddress) => {
+    // TODO: actual validation of address
+    return true;
+}
+
+var processBlock = (minerData) => {
+    var block = node.miningJobs[minerData.address];
+
+    block.nonce = minerData.nonce;
+    block.dateCreated = minerData.dateCreated;
+    block.blockHash = minerData.blockHash;
+
+    var blockHash = CryptoJS.SHA256("" + block.blockDataHash + nonce).toString();
+    if(blockHash.substring(0, DIFFICULTY) == Array(DIFFICULTY + 1).join("0"))
+    {
+        node.blocks.push(block);
+
+        block.transactions.forEach(t =>
+                        node.pendingTransactions.splice(node.pendingTransactions.indexOf(t['transactionHash']), 1)); //TODO: CHECK SYNTAX
+
+        block.transactions.forEach(function(t) {
+            node.balances[t['fromAddress']] -= t['value'];
+            node.baalnces[t['toAddress']] += t['value'];
+        });
+    }
+    else
+    {
+        return false;
+    }
+}
+
+var createPendingTransaction = (transactionData) => {
+    var hasMoneyForTransaction =
+        addressHasMoney(transactionData.fromAddress, transactionData.value);
+    var keysAreValid = validateKeys(transactionData.senderPubKey, transactionData.senderSignature);
+    var addressesAreValid = validateAddresses(transactionData.fromAddress, transactionData.toAddress);
+
+    if(hasMoneyForTransaction && keysAreValid && addressesAreValid)
+    {
+        return new Transaction(transactionData.fromAddress,
+                               transactionData.toAddress,
+                               transactionData.value,
+                               transactionData.senderPubKey,
+                               transactionData.senderSignature);
+    }
+    else
+    {
+        // TODO: Throw exception
+        return false;
+    }
+}
+
+var synchronizeWithPeer = () => {
+
+}
+
+var addPeer = (peerData) => {
+
 }
 
 startNode();
