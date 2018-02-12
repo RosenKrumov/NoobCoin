d2767173d838daad110ebb8711f9946e75a65b89
RosenKrumov <rosenkrumov95@gmail.com>
2018-02-12 08:09:22 -0800

    Added new version of block explorer with node js web server and pug views

--- noobexplorer/noobexplorer.js
+++ noobexplorer/noobexplorer.js
@@ -2,34 +2,111 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var http = require('http');
+var express = require('express');
+var path = require('path');
 
 var node_hostname = "localhost"
-var node_port = "3001"
+var http_port = "8001"
 
-function getLatestBlocks() {
-	var options = {
-	  host: 'localhost',
-	  port: 3001,
-	  path: '/blocks'
-	};
+var initHttpServer = () => {
+	var app = express();
+	app.set('view engine', 'pug');
+	app.use(bodyParser.json());
 	
-	var data = "";
+	app.get('/', function(req, res){
+		var options = {
+		  host: 'localhost',
+		  port: 3001,
+		  path: '/blocks'
+		};
 
-	http.get(options, function(res) {
-	  console.log("Got response: " + res.statusCode);
+		http.get(options, function(response) {
+		  console.log("Got response: " + response.statusCode);
 
-	  res.on("data", function(chunk) {
-	  	data += chunk;
-		console.log("BODY: " + chunk);
-	  });
-	}).on('error', function(e) {
-	  console.log("Got error: " + e.message);
+		  response.on("data", function(chunk) {
+		  	var dataStr = ("" + chunk);
+		  	var dataJSON = JSON.parse(dataStr);
+			res.render('index', {title: 'Hey', message: 'Noob Explorer', data: dataJSON});
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		});
 	});
 	
-	var blockDiv = document.createElement('div');
-	blockDiv.innerHTML += data;
+	app.get('/blocks/:id', function(req, res){
+		var options = {
+		  host: 'localhost',
+		  port: 3001,
+		  path: '/blocks'
+		};
+		var index = req.params.id;
+
+		http.get(options, function(response) {
+		  console.log("Got response: " + response.statusCode);
+
+		  response.on("data", function(chunk) {
+		  	var dataStr = ("" + chunk);
+		  	var dataJSON = JSON.parse(dataStr);
+			res.render('block', {title: 'Hey', message: 'Noob Explorer', block: dataJSON[index]});
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		});
+	});
 	
-	document.getElementById('blocks').appendChild(blockDiv);
+	app.get('/transactions/:hash', function(req, res){
+		var hash = req.params.hash;
+		var options = {
+		  host: 'localhost',
+		  port: 3001,
+		  path: '/transaction/' + hash
+		};
+
+		http.get(options, function(response) {
+		  console.log("Got response: " + response.statusCode);
+
+		  response.on("data", function(chunk) {
+		  	var dataStr = ("" + chunk);
+		  	var dataJSON = JSON.parse(dataStr);
+			res.render('transaction', {title: 'Hey', message: 'Noob Explorer', transaction: dataJSON});
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		});
+	});
+	
+	app.get('/blocks/:id/transactions', function(req, res){
+		var id = req.params.id;
+		var options = {
+		  host: 'localhost',
+		  port: 3001,
+		  path: '/blocks'
+		};
+
+		http.get(options, function(response) {
+		  console.log("Got response: " + response.statusCode);
+
+		  response.on("data", function(chunk) {
+		  	var dataStr = ("" + chunk);
+		  	var dataJSON = JSON.parse(dataStr);
+			res.render('block-transactions', {title: 'Hey', message: 'Noob Explorer', block: dataJSON[id]});
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		});
+
+	});
+	
+	app.listen(http_port, () => console.log('Listening http on port ' + http_port));
 }
 
+function getLatestBlocks() {
+
+	
+	console.log('DATA: ' + data);
+
+	return data;
+}
+
+initHttpServer();
 
